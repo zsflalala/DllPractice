14834ba2b9a2efffea79bdb3a8fe6c3def3a9cf0
zsflalala <73597361+zsflalala@users.noreply.github.com>
2023-08-25 23:46:35 +0800

    renew customer need
    更新客户需求，空字符串不入内

--- DemoDll/MyStringDll.cpp
+++ DemoDll/MyStringDll.cpp
@@ -5,13 +5,20 @@ std::vector<std::string> splitString(const std::string& vInput, char vSeperator)
 {
 	std::vector<std::string> result;
 	int subIndex = 0;
+	std::string cutString,lastString;
 	for (int i = 0; i < vInput.size(); i++)
 	{
 		if (vInput[i] == vSeperator) {
-			result.push_back(vInput.substr(subIndex, i - subIndex));
+			cutString = vInput.substr(subIndex, i - subIndex);
+			if (cutString != "") {
+				result.push_back(cutString);
+			}
 			subIndex = i + 1;
 		}
 	}
-	result.push_back(vInput.substr(subIndex));
+	lastString = vInput.substr(subIndex);
+	if (lastString != "") {
+		result.push_back(lastString);
+	}
 	return result;
 }
--- DemoInvokeStringDll/test.cpp
+++ DemoInvokeStringDll/test.cpp
@@ -3,35 +3,35 @@
 
 TEST(SpringTestCase, NormalSplit1)
 {
-	std::vector<std::string> res{"dadf", "sdfe", "few"};
-	std::vector<std::string> testRes = splitString("dadf,sdfe,few", ',');
+	std::vector<std::string> res{"Hello", "C++", "World"};
+	std::vector<std::string> testRes = splitString("Hello,C++,World", ',');
 	ASSERT_EQ(res, testRes);
 }
 
 TEST(SpringTestCase, NormalSplit2)
 {
-	std::vector<std::string> res{"z", "", "s", "", "f"};
-	std::vector<std::string> testRes = splitString("z,,s,,f", ',');
+	std::vector<std::string> res{"Hello", "C++", "World"};
+	std::vector<std::string> testRes = splitString("Hello,,C++,,World", ',');
 	ASSERT_EQ(res, testRes);
 }
 
 TEST(SpringTestCase, EmptySplit1)
 {
-	std::vector<std::string> res{"", "", "Hello", "World!"};
+	std::vector<std::string> res{"Hello", "World!"};
 	std::vector<std::string> testRes = splitString("  Hello World!", ' ');
 	ASSERT_EQ(res, testRes);
 }
 
 TEST(SpringTestCase, EmptySplit2)
 {
-	std::vector<std::string> res{"", "", "", "", ""};
+	std::vector<std::string> res{};
 	std::vector<std::string> testRes = splitString("    ", ' ');
 	ASSERT_EQ(res, testRes);
 }
 
 TEST(SpringTestCase, EmptyString)
 {
-	std::vector<std::string> res{""};
+	std::vector<std::string> res{};
 	std::vector<std::string> testRes = splitString("", ',');
 	ASSERT_EQ(res, testRes);
 }
